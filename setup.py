cc04d56b2ad1e1763e6d1101f2aa7b1dc5175f4a
Pasindu <pasindusandakan@gmail.com>
2024-05-02 15:29:30 +0530

    template added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'productDetection01',
+    version= '0.0.0',
+    author= 'pasindu',
+    author_email= '',
+    packages= find_packages(),
+    install_requires = []
+
+)
